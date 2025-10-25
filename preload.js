6104b199d0f77737d8f7489ee1fb23a013d7e0b9
Ikigai <ibosana02@gmail.com>
2025-10-25 14:38:44 +0530

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-// preload.js
-
-// All of the Node.js APIs are available in the preload process.
-// It has the same sandbox as a Chrome extension.
-// This script runs before the renderer process (your HTML page) is loaded.
-// You can use it to securely expose a limited set of Node.js APIs to your
-// renderer process. For this simple app, we don't need to expose anything,
-// but it's good practice to have this file.
-console.log("Preload script loaded.");
